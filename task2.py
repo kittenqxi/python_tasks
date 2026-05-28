cd53489f235f6f5252f1394f939afada13fc152b
Sonya Pesh <sonypeshevich2007@gmail.com>
2026-05-28 20:30:16 +0300

    add new task two

--- task2.py
+++ task2.py
@@ -1,11 +1,27 @@
-matrix=[[5, 1, 9], [2, 11, 3], [7, 4, 6]]
-maxx=matrix[0][0]
+# matrix=[[5, 1, 9], [2, 11, 3], [7, 4, 6]]
+# maxx=matrix[0][0]
+# i=0
+# while i<len(matrix):
+#     j=0
+#     while j<len(matrix[i]):
+#         if matrix[i][j]>maxx:
+#             maxx=matrix[i][j]
+#         j+=1
+#     i+=1
+# print(maxx)
+
+a=int(input())
+b=int(input())
+maxx=0
 i=0
-while i<len(matrix):
+while i<a:
     j=0
-    while j<len(matrix[i]):
-        if matrix[i][j]>maxx:
-            maxx=matrix[i][j]
+    while j<b:
+        num=int(input())
+        if i==0 and j==0:
+            maxx=num
+        if num>maxx:
+            maxx=num
         j+=1
     i+=1
-print(maxx)
+print('Maximum element: ', maxx)

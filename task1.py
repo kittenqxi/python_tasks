4eb8a2ecb6dabefc274e501d187afeb960f79768
Sonya Pesh <sonypeshevich2007@gmail.com>
2026-05-28 20:20:01 +0300

    add new task one

--- task1.py
+++ task1.py
@@ -1,10 +1,23 @@
-matrix=[[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+# matrix=[[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+# total_sum=0
+# i=0
+# while i<len(matrix):
+#     j=0
+#     while j<len(matrix[i]):
+#         total_sum+=matrix[i][j]
+#         j+=1
+#     i+=1
+# print(total_sum)
+
+a=int(input())
+b=int(input())
 total_sum=0
 i=0
-while i<len(matrix):
+while i<a:
     j=0
-    while j<len(matrix[i]):
-        total_sum+=matrix[i][j]
+    while j<b:
+        num=int(input())
+        total_sum+=num
         j+=1
     i+=1
 print(total_sum)
